7e335d3891b648ed9c60c051809fda62ec203896
Roli Singh <152026651+rolisingh1803@users.noreply.github.com>
2024-05-01 22:25:42 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoApplication.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace ToDoApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+              DbToDo db = new DbToDo();
+        // GET: Home
+        public ActionResult Index(ToDoMdl td)
+        {
+          
+            if(Session["id"] != null )
+            {
+                int userid = Convert.ToInt32(Session["id"]);
+                int id = Convert.ToInt32(Session["todoid"]);
+                return View(db.GetToDo(userid));
+            }
+            else
+            {
+                RedirectToAction("login", "Login");
+            }
+            return View();
+        }
+
+        public JsonResult List()
+        {
+           
+
+            int userid = Convert.ToInt32(Session["id"]);
+            return Json(db.GetToDo(userid), JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult Add(ToDoMdl td)
+        {
+            td.Id = Convert.ToInt32(Session["id"]);
+            return Json(db.AddToDo(td), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetbyId(int id)
+        {
+            int userid = Convert.ToInt32(Session["id"]);
+            var ToDoMdl = db.GetToDo(userid).Find(x => x.Id.Equals(id));
+
+            return Json(ToDoMdl, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Update(ToDoMdl td)
+        {
+
+            td.userid = Convert.ToInt32(Session["id"]);
+           
+            return Json(db.UpdateToDo(td), JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult Delete(int Id)
+        {
+            
+           return Json(db.DeleteToDo(Id), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Checked(ToDoMdl td)
+        {
+            //td.id = Convert.ToInt32(Session["id"]);
+            return Json(db.checkedtodo(td),JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ToDoApplication.Models;
+using System.Configuration;
+
+namespace ToDoApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        //[Authorize]
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+       
+
+        [HttpGet]
+        public ActionResult register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult register(User us)
+        {
+           
+            if (ModelState.IsValid)
+
+            {
+                
+                if (!IsUserExist(us.Email))
+                {
+                   
+                    {
+                        DbToDo db = new DbToDo();
+                        db.Register(us);
+                        ModelState.Clear();
+                        
+                        ViewBag.SuccessMessage = "Registration Successfully";
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User with same email already exist!");
+                }
+                
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult login()
+        {
+            //DbToDo db = new DbToDo();
+            //string data = db.Register().Find(reg => reg.Id == id);             
+          
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+
+        public ActionResult login(User2 us)
+        {
+           // DbToDo db = new DbToDo();
+            if (ModelState.IsValid)
+            {
+                if (IsValidUser(us.Email, us.Password))
+                {
+                  
+                       //Session["firstname"] = user.FirstName.ToString();
+                       //return RedirectToAction("UserDashBoard");
+                    
+                    return RedirectToAction("Index","Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or Password is wrong");
+                }
+            }
+            return View();
+        }
+
+     
+        public ActionResult logout()
+        {
+            //FormsAuthentication.SignOut();
+            //FormsAuthentication.RedirectFromloginPage();
+           // HttpContext.Current.Session.Clear();
+            //Session.Abandon();
+            return RedirectToAction("index", "Login");
+        }
+
+
+        private bool IsValidUser(string email,string password)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["conn"].ToString();
+            SqlConnection con = new SqlConnection(constring);
+          
+            bool IsValid = false;
+            //string query = "select id from Registration where email=@email and password=@password and id=@id ";
+            SqlCommand cmd = new SqlCommand("proc_login", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@password", password);
+        
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+               
+            con.Open();
+           // int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (dt.Rows.Count > 0)
+            {     
+                Session["id"] = dt.Rows[0]["id"];
+                IsValid =true;
+            }
+            return IsValid;
+           
+        }
+
+
+
+        // check user is exist or not 
+
+        private bool IsUserExist(string email)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["conn"].ToString();
+            SqlConnection con = new SqlConnection(constring);
+            bool IsUserExist = false;
+            string query = "select * from Registration where email=@email";
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue("@email", email);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                con.Open();
+               // int i = cmd.ExecuteNonQuery();
+                con.Close();
+                if (dt.Rows.Count > 0)
+                {
+                    IsUserExist = true;
+                }
+            }
+            return IsUserExist;
+        }
+
+
+       
+
+    }
+}
