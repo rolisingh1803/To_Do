133ab6551ac7d45b1f4746e56d1c8064eae6fdee
Roli Singh <152026651+rolisingh1803@users.noreply.github.com>
2024-05-01 22:22:19 +0530

    Add files via upload

---
+++ Scripts/ToDo.js
@@ -0,0 +1,255 @@
+﻿
+
+
+$(document).ready(function () {
+  
+    loadData();
+});
+
+function loadData() {
+
+    $.ajax({
+       
+        url: "/Home/List/id",
+        type: "GET",
+        contentType: "application/json;charset=urf-8",
+        dataType: "json",
+        success: function (result) {
+            
+            var html = '';
+            $.each(result, function (key, item) {
+                console.log(item.Checkbox);
+                html += '<tr class="warning">';
+                if (item.Checkbox == true) {
+                    html += '<td> <input type="checkbox" id="check' + item.Id + '" name="row-check" class="item-completed" checked onclick="Check(' + item.Id + ')" ></td>'
+                    html += '<td class="checkedStatus">' + item.Title + '</td>';
+                    html += '<td class="checkedStatus">' + item.Description + '</td>';
+                    html += '<td class="checkedStatus">' + item.strCreatedDate + '</td>';
+                    html += '<td id="upd" class="checkedStatus">' + item.strUpdatedDate + '</td>';
+                  
+                    html += '<td>  <button class="btn btn-success">Edit</button> | <button class="btn btn-danger" href="#" onclick="Delete( ' + item.Id + ')">Delete</button>'
+                    '</td>';  
+                   
+                }
+                else {
+                    html += '<td> <input type="checkbox" id="check' + item.Id + '" name="row-check" class="item-completed"  onclick="Check(' + item.Id + ')" ></td>'
+                    html += '<td> ' + item.Title + '</td>';
+                    html += '<td>' + item.Description + '</td>';
+                    html += '<td>' + item.strCreatedDate + '</td>';
+                    html += '<td>' + item.strUpdatedDate + '</td>';
+                 
+                    html += '<td>  <button class="btn btn-success" onclick="return getbyId( ' + item.Id + ')">Edit</button> | <button class="btn btn-danger" href="#" onclick="Delete( ' + item.Id + ')">Delete</button>'
+                    '</td>';  
+                }
+               
+             
+
+                html += '</tr>';
+                $('.tbody').html(html);
+               
+               
+            });
+           
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+
+
+
+
+function Add() {
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var ToDoObj = {
+
+        Title:$('#title').val(),
+        Description: $('#description').val()
+       
+      
+    };
+    $.ajax({
+        url: "/Home/Add",
+        data: JSON.stringify(ToDoObj),
+        type: "Post",
+        contentType: "application/json;charset=urf-8",
+        dataType: "json",
+        success: function (result) {
+            if (result == true) {
+                alert("data added successfully");
+                $('#myModal').modal('hide');
+            }
+            else {
+                alert("some error occured");
+               
+            }
+            
+            loadData();
+           
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+
+
+function getbyId(id) {
+    $('#title').css('border-color', 'lightgrey');
+    $('#description').css('border-color', 'lightgrey');
+
+    $.ajax({
+        url: "/Home/getbyId/" + id,
+        type: "GET",
+        contentType: "application/json;chatset=UTF-8",
+        dataType: "json",
+        success: function (result) {
+            $('#Id').val(result.Id);
+            
+                
+           $('#title').val(result.Title);
+           $('#description').val(result.Description);
+            
+            $('#myModal').modal('show');
+            $('#btnUpdate').show();
+            $('#btnAdd').hide();
+            $('#TitleAdd').hide();
+
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+
+
+function Update() {
+   
+    var res = validate();
+    if (res == false) {
+        return false;
+    }
+    var todoObj = {
+        Id: $('#Id').val(),
+        Title: $('#title').val(),
+        Description: $('#description').val(),
+              
+        
+    };
+    $.ajax({
+        url: "/Home/Update",
+        data: JSON.stringify(todoObj),
+        type: "POST",        
+        contentType: "application/json;charset=urf-8",
+        dataType: "json",
+        success: function (result) {
+            if (result == true) {
+              
+                alert("updated successfully");
+    
+                $('#myModal').modal('hide');
+            }
+            else {
+                alert("some error occured");
+
+            }
+            
+            $("Id").val("");
+            $("title").val("");
+            $("description").val("");
+            loadData();
+        },
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+}
+
+
+function Delete(id) {
+    var ans = confirm("Are you sure you wants to delete.");
+    if (ans) {
+        $.ajax({
+            url: "/Home/Delete/" + id,
+            type: "POST",
+            contentType: "application/json;charset=UTF-8",
+            dataType: "json",
+            success: function (result) {
+                loadData();
+            },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+}
+
+function clearTextBox() {
+    $('#title').val("");
+    $('#description').val("");
+    $('#btnUpdate').hide();
+    $('#TitleUpd').hide();
+    $('#btnAdd').show();
+    $('#title').css('border-color', 'lightgrey');
+    $('#description').css('border-color', 'lightgrey');
+}
+
+
+function validate() {
+    var isValid = true; 
+    if ($('#title').val().trim() == "") {
+        $('#title').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#title').css('border-color', 'lightgrey');
+    }
+    if ($('#description').val().trim() == "") {
+        $('#description').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#description').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
+
+
+
+function Check(value) {
+    console.log("check", value);
+    let selector = "#check" + value;
+    let obj = {
+        "Id": value,
+        "Checkbox": false
+    }
+    if ($(selector).is(':checked')) {
+        obj.Checkbox = true;
+    }
+    else {
+        obj.Checkbox = false
+    }
+
+
+    $.ajax({
+        url: "/Home/checked",
+        type: "POST",
+        data: JSON.stringify(obj),
+        contentType: "application/json;charset=urf-8",
+        dataType: "json",
+        success: function (result) {
+            if (result == true) {
+                alert("Data updated successfully");
+            }
+        }
+    })
+    loadData();
+}
+
---
+++ Scripts/register.js
@@ -0,0 +1,131 @@
+﻿//function clearTextBox() {
+//    $("#firstname").val("");
+//    $("#lastname").val("");
+//    $("#email").val("");
+//    $("#address").val("");
+//    $("#gender").val("");
+//    $("#contact").val("");
+//    $("#dob").val("");
+//    $("#password").val("");
+//    $("#confirm").val("");
+ 
+//    $('#firstname').css('border-color', 'lightgrey');
+//    $('#lastname').css('border-color', 'lightgrey');
+//}
+
+
+
+function registered() {
+    var res = validate();
+    if (res = false) {
+        return false;
+    }
+
+    var regObj = {
+        FirstName: $("#firstname").val(),
+        LastName: $("#lastname").val(),
+        Email: $("#email").val(),
+        Address: $("#address").val(),
+        Gender: $("#gender").val(),
+        Contact: $("#contact").val(),
+        DOB: $("#dob").val(),
+        Password: $("#password").val(),
+        Confirm: $("#confirm").val()
+    };
+
+    $.ajax({
+        url: "/Login/index",
+        datatype: "json",
+        contentType: "application/json;charset=urf-8",
+        type: "POST",
+        data: JSON.stringify(regObj),
+        success: function (result) {
+            if (result == true) {
+                console.log(result);
+                alert("Registered Successfully");
+            } else {
+                alert("something wrong");
+            }
+        },
+    
+        error: function (errormessage) {
+            alert(errormessage.responseText);
+        }
+    });
+
+}
+
+
+
+
+
+
+
+function validate() {
+    var isValid = true;
+    if ($('#firstname').val().trim() == "") {
+        $('#firstname').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#firstname').css('border-color', 'lightgrey');
+    }
+    if ($('#lastname').val().trim() == "") {
+        $('#lastname').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#lastname').css('border-color', 'lightgrey');
+    }
+    if ($('#email').val().trim() == "") {
+        $('#email').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#email').css('border-color', 'lightgrey');
+
+    }
+    if ($('#address').val().trim() == "") {
+        $('#address').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#address').css('border-color', 'lightgrey');
+    }
+    if ($('#gender').val().trim() == "") {
+        $('#gedner').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#gender').css('border-color', 'lightgrey');
+    }
+    if ($('#contact').val().trim() == "") {
+        $('#contect').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#contact').css('border-color', 'lightgrey');
+    }
+    if ($('#dob').val().trim() == "") {
+        $('#dob').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#dob').css('border-color', 'lightgrey');
+    }
+    if ($('#password').val().trim() == "") {
+        $('#password').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#password').css('border-color', 'lightgrey');
+    }
+    if ($('#confirm').val().trim() == "") {
+        $('#confirm').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+        $('#confirm').css('border-color', 'lightgrey');
+    }
+    return isValid;
+}
